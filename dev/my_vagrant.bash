c7ee151ad86cacd8bf3cecbd8901808e0a14b68a
Andrew Cassell <cassell@users.noreply.github.com>
2015-11-06 22:40:26 -0500

    dev stuff

--- dev/my_vagrant.bash
+++ dev/my_vagrant.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo ln -s /srv/www/devq/server/apache_dev_vhost.conf /etc/httpd/sites-enabled/devq.conf
+sudo ln -s /srv/www/devq/server/apache_dev/apache_dev_vhost.conf /etc/httpd/sites-enabled/devq.conf
 sudo ln -s /etc/httpd/sites-enabled/devq.conf /etc/httpd/sites-enabled/devq.conf
 sudo /bin/systemctl restart  httpd.service
